dfc8c646d156622dc35c2803153bc79de6664854
krishnanjalivu <vukrishnanjali@gmail.com>
2023-09-12 22:01:12 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,42 @@ console.log(error);
 });
 
 
-app.get("/",(req,res)=>{
+app.get("/books",async(req,res)=>{
+    try{
+        const books = await Book.find({});
+        return res.status(201).json(
+            {
+                count:books.length,
+                data:books
+            }
+        );
+
+    }
+    catch(error)
+    {
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
    
-    console.log("hello world");
-    return res.status(234).send("Welcome to MERN Stack")
+    // console.log("hello world");
+    // return res.status(234).send("Welcome to MERN Stack")
+});
+app.get("/books/:id",async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const book = await Book.findById(id);
+        return res.status(201).json(book
+        );
+
+    }
+    catch(error)
+    {
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
+   
+    // console.log("hello world");
+    // return res.status(234).send("Welcome to MERN Stack")
 });
 app.post("/books",async(req,res)=>{
 try{
